a5fe30caf42ea5e5919b9e5d30bca3c5f544373a
ushakalapande@gmail.com <ushakalapande2gmail.com>
2022-11-06 21:35:49 +0530

    Class file name ListOutputPop changes because this code are correct

--- Maven4_Amazon/src/main/java/module/ListOutpopUp.java
+++ Maven4_Amazon/src/main/java/module/ListOutpopUp.java
@@ -8,27 +8,27 @@ import org.openqa.selenium.support.PageFactory;
 public class ListOutpopUp {
 
 	
-//	@FindBy(xpath= "//input[@id='list-name']") 
-//	private WebElement enterlistname;
-//	
-//	@FindBy(xpath= "//span[text()='Create List']") 
-//	private WebElement createlist;
-//	
-//	
-//	public ListOutpopUp(WebDriver driver)
-//	{
-//		PageFactory.initElements(driver, this);
-//	}
-//	
-//	public void clickonList1()
-//	{
-//		enterlistname.clear();
-//		enterlistname.sendKeys("product1");
-//	}
-//	public void createList()
-//	{
-//		createlist.click();
-//	}
+	@FindBy(xpath= "//input[@id='list-name']") 
+	private WebElement enterlistname;
+	
+	@FindBy(xpath= "//span[text()='Create List']") 
+	private WebElement createlist;
+	
+	
+	public ListOutpopUp(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void clickonList1()
+	{
+		enterlistname.clear();
+		enterlistname.sendKeys("product1");
+	}
+	public void createList()
+	{
+		createlist.click();
+	}
 	
 
 	}
